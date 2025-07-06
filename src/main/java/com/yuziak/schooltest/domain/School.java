570d685c6b974569eee431705484b4515a9b3f6d
Daniil <daniil.yuziak@gmail.com>
2025-07-06 16:46:44 +0300

    explicitly added getters, setters and builder to avoid problems when launching the project

--- src/main/java/com/yuziak/schooltest/domain/School.java
+++ src/main/java/com/yuziak/schooltest/domain/School.java
@@ -2,15 +2,9 @@ package com.yuziak.schooltest.domain;
 
 
 import jakarta.persistence.*;
-import lombok.*;
 
 @Entity
 @Table(name = "schools")
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
 public class School {
 
     @Id
@@ -29,4 +23,98 @@ public class School {
     @Column(name = "is_active")
     private boolean isActive = true;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEdrpou() {
+        return edrpou;
+    }
+
+    public void setEdrpou(String edrpou) {
+        this.edrpou = edrpou;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public SchoolType getType() {
+        return type;
+    }
+
+    public void setType(SchoolType type) {
+        this.type = type;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+        private final School school;
+
+        public Builder() {
+            school = new School();
+        }
+
+        public Builder id(Long id) {
+            school.setId(id);
+            return this;
+        }
+
+        public Builder name(String name) {
+            school.setName(name);
+            return this;
+        }
+
+        public Builder edrpou(String edrpou) {
+            school.setEdrpou(edrpou);
+            return this;
+        }
+
+        public Builder region(String region) {
+            school.setRegion(region);
+            return this;
+        }
+
+        public Builder type(SchoolType type) {
+            school.setType(type);
+            return this;
+        }
+
+        public Builder isActive(boolean isActive) {
+            school.setActive(isActive);
+            return this;
+        }
+
+        public School build() {
+            return school;
+        }
+    }
 }
